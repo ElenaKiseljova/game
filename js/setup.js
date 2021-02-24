3b96db7ca83a84d171dd4809bb249aa7e007ac35
Lena <e.a.kiseljova@gmail.com>
2021-02-24 22:59:30 +0200

    Выполнила четвертое задание

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,7 @@ var FIRST_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', '
 var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COLOR_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var СOLOR_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var COLOR_FIREBALLS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var ENTER_KEY_CODE = 13;
 var ESC_KEY_CODE = 27;
@@ -90,16 +91,42 @@ setupSubmit.addEventListener('blur', function () {
   document.removeEventListener('keydown', onEnterPress, true);
 });
 
-var similarListElement = document.querySelector('.setup-similar-list');
-var templateGamer = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
-
 var randomCoefficient = function (parameter) {
   var coefficient = Math.trunc(Math.random() * parameter.length);
 
   return coefficient;
 };
+
+
+var setupColorCoat = setup.querySelector('.wizard-coat');
+var setupColorFireball = setup.querySelector('.setup-fireball-wrap');
+var setupColorEyes = setup.querySelector('.wizard-eyes');
+var inputColorCoat = setup.querySelector('input[name="coat-color"]');
+var inputColorEyes = setup.querySelector('input[name="eyes-color"]');
+var inputColorFireball = setup.querySelector('input[name="fireball-color"]');
+
+setupColorCoat.addEventListener('click', function () {
+  let color = COLOR_COATS[randomCoefficient(COLOR_COATS)];
+  setupColorCoat.style.fill = color;
+  inputColorCoat.value = color;
+});
+
+setupColorFireball.addEventListener('click', function () {
+  let color = COLOR_FIREBALLS[randomCoefficient(COLOR_FIREBALLS)];
+  setupColorFireball.style.backgroundColor = color;
+  inputColorFireball.value = color;
+});
+
+setupColorEyes.addEventListener('click', function () {
+  let color = СOLOR_EYES[randomCoefficient(СOLOR_EYES)];
+  setupColorEyes.style.fill = color;
+  inputColorEyes.value = color;
+});
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var templateGamer = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
 
 var similarGamers = [];
 

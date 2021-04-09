5cb0ceec3d1b687ba3149e261fb65925bd72d436
Lena <e.a.kiseljova@gmail.com>
2021-04-09 18:21:09 +0300

    Разбила код на Модули

---
+++ js/util.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*(function () {
+  var ENTER_KEYCODE = 13;
+  var ESC_KEYCODE = 27;
+
+  window.util = {
+    isEscEvent : function (evt, action) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        action();
+      }
+    },
+    isEnterEvent : function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
+    }
+  };
+})();*/
+
+window.util = (function () {
+  var ENTER_KEYCODE = 13;
+  var ESC_KEYCODE = 27;
+
+  return {
+    isEscEvent : function (evt, action) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        action();
+      }
+    },
+    isEnterEvent : function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
+    }
+  };
+})();

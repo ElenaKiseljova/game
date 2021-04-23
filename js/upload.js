6fc8f60fa4e174beaf105e351496275634d06b20
Lena <e.a.kiseljova@gmail.com>
2021-04-23 22:51:45 +0300

    Выаолнила первую часть задания. Без создания файла backend.js

---
+++ js/upload.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+  var URL = 'http://game.webraido.space/user.php';
+
+  window.upload = function (data, onSuccess) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseTyte = 'json';
+
+    xhr.addEventListener('load', function (evt) {
+      onSuccess(xhr.response);
+    });
+
+    xhr.open('POST', URL);
+
+    xhr.send(data);
+  };
+})();
